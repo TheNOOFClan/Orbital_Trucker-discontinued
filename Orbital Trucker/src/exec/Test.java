ec646216a52bcfd939d95ee18f7d7d90a10fdc64
onionsans <artem.astapchuk@gmail.com>
2016-10-03 23:01:32 -0700

    Messing with LWJGL

--- Orbital Trucker/src/exec/Test.java
+++ Orbital Trucker/src/exec/Test.java
@@ -1,10 +1,36 @@
 package exec;
 
-public class Test {
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class Test
+{
 
-	}
+    public void start()
+    {
+        try
+        {
+            Display.setDisplayMode(new DisplayMode(160, 144));
+            Display.create();
+        } catch (LWJGLException e)
+        {
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        while (!Display.isCloseRequested())
+        {
+            Display.update();
+        }
+
+        Display.destroy();
+    }
+
+    public static void main(String[] args)
+    {
+        Test test = new Test();
+        test.start();
+    }
 
 }
