9811a10a87d06c5dc4acc324ec21ef11d5f5af05
onionsans <artem.astapchuk@gmail.com>
2016-10-05 03:25:02 -0700

    Basic Engine

--- Orbital Trucker/src/exec/Test.java
+++ Orbital Trucker/src/exec/Test.java
@@ -1,36 +1,32 @@
 package exec;
 
-import java.io.IOException;
 import org.lwjgl.LWJGLException;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 import org.lwjgl.opengl.GL11;
-import org.newdawn.slick.Color;
-import org.newdawn.slick.opengl.Texture;
-import org.newdawn.slick.opengl.TextureLoader;
-import org.newdawn.slick.util.ResourceLoader;
+import obj.Ship;
 
 public class Test
 {
 
-    /** The texture that will hold the image details */
-    private Texture texture;
+    private int scale = 2;
 
-    /**
-     * Start the example
-     */
+    private Ship ship;
+
     public void start()
     {
-        initGL(800, 600);
-        init();
+        initGL(160 * scale, 144 * scale);
+        ship = new Ship("truck/");
+        Ship.scale = scale;
 
         while (true)
         {
+            ship.update();
             GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
             render();
 
             Display.update();
-            Display.sync(100);
+            Display.sync(60);
 
             if (Display.isCloseRequested())
             {
@@ -79,46 +75,11 @@ public class Test
     }
 
     /**
-     * Initialise resources
-     */
-    public void init()
-    {
-
-        try
-        {
-            // load texture from PNG file
-            texture = TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream("res/ship0.png"));
-
-            System.out.println("Texture loaded: " + texture);
-            System.out.println(">> Image width: " + texture.getImageWidth());
-            System.out.println(">> Image height: " + texture.getImageHeight());
-            System.out.println(">> Texture width: " + texture.getTextureWidth());
-            System.out.println(">> Texture height: " + texture.getTextureHeight());
-            System.out.println(">> Texture ID: " + texture.getTextureID());
-        } catch (IOException e)
-        {
-            e.printStackTrace();
-        }
-    }
-
-    /**
      * draw a quad with the image on it
      */
     public void render()
     {
-        Color.white.bind();
-        texture.bind(); // or GL11.glBind(texture.getTextureID());
-
-        GL11.glBegin(GL11.GL_QUADS);
-        GL11.glTexCoord2f(0, 0);
-        GL11.glVertex2f(100, 100);
-        GL11.glTexCoord2f(1, 0);
-        GL11.glVertex2f(100 + texture.getTextureWidth(), 100);
-        GL11.glTexCoord2f(1, 1);
-        GL11.glVertex2f(100 + texture.getTextureWidth(), 100 + texture.getTextureHeight());
-        GL11.glTexCoord2f(0, 1);
-        GL11.glVertex2f(100, 100 + texture.getTextureHeight());
-        GL11.glEnd();
+        ship.render();
     }
 
     /**
@@ -128,5 +89,6 @@ public class Test
     {
         Test textureExample = new Test();
         textureExample.start();
+
     }
 }
---
+++ Orbital Trucker/src/obj/Ship.java
@@ -0,0 +1,96 @@
+package obj;
+
+import java.io.IOException;
+
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.GL11;
+import org.newdawn.slick.Color;
+import org.newdawn.slick.opengl.Texture;
+import org.newdawn.slick.opengl.TextureLoader;
+import org.newdawn.slick.util.ResourceLoader;
+
+public class Ship
+{
+    private static final int MAX_MOVE_TIMEOUT = 10;
+    private static final int MAX_TURN_TIMEOUT = 30;
+
+    private Texture[] textures = new Texture[8];
+
+    private float x;
+    private float y;
+
+    private int move_timeout;
+    private int turn_timeout;
+
+    public static int scale;
+
+    private int dir;
+
+    public Ship(String texture_dir)
+    {
+        try
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                textures[i] = TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream("res/" + texture_dir + Integer.toString(i) + ".png"));
+            }
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+            Display.destroy();
+            System.exit(1);
+        }
+
+        x = 0;
+        y = 0;
+        dir = 0;
+        move_timeout = 0;
+        turn_timeout = 0;
+    }
+
+    public void render()
+    {
+        Color.white.bind();
+        textures[dir].bind(); // or GL11.glBind(texture.getTextureID());
+
+        GL11.glBegin(GL11.GL_QUADS);
+        GL11.glTexCoord2f(0, 0);
+        GL11.glVertex2f((int) (x * scale), (int) (y * scale));
+        GL11.glTexCoord2f(1, 0);
+        GL11.glVertex2f((int) ((x + textures[dir].getTextureWidth()) * scale), (int) (y * scale));
+        GL11.glTexCoord2f(1, 1);
+        GL11.glVertex2f((int) ((x + textures[dir].getTextureWidth()) * scale), (int) ((y + textures[dir].getTextureHeight()) * scale));
+        GL11.glTexCoord2f(0, 1);
+        GL11.glVertex2f((int) (x * scale), (int) ((y + textures[dir].getTextureHeight()) * scale));
+        GL11.glEnd();
+    }
+
+    public void update()
+    {
+
+        if (move_timeout > 0)
+            move_timeout--;
+
+        if (turn_timeout > 0)
+            turn_timeout--;
+        if (Keyboard.isKeyDown(Keyboard.KEY_LEFT) && turn_timeout == 0)
+        {
+            dir = (dir + 7) % 8;
+            turn_timeout = MAX_TURN_TIMEOUT;
+        } else
+            if (Keyboard.isKeyDown(Keyboard.KEY_RIGHT) && turn_timeout == 0)
+            {
+                dir = (dir + 1) % 8;
+                turn_timeout = MAX_TURN_TIMEOUT;
+            }
+        if (Keyboard.isKeyDown(Keyboard.KEY_UP) && move_timeout == 0)
+        {
+            x += Math.cos(dir * Math.PI / 4);
+            y += Math.sin(dir * Math.PI / 4);
+            move_timeout = MAX_MOVE_TIMEOUT;
+        }
+
+    }
+
+}
