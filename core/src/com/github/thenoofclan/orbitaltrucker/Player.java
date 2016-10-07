e1ec4dccc1687fe0f6514f8c00fed305e8895420
onionsans <artem.astapchuk@gmail.com>
2016-10-07 01:34:12 -0700

    The code is now slightly less broken than it was the last time I checked.

--- core/src/com/github/thenoofclan/orbitaltrucker/Player.java
+++ core/src/com/github/thenoofclan/orbitaltrucker/Player.java
@@ -1,118 +1,93 @@
 package com.github.thenoofclan.orbitaltrucker;
 
-
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.math.Vector2;
 
 public class Player extends Ship
 {
-	private Texture spriteTexture;
-	public Player(Texture texture, Texture texture45, int width, int height, int x, int y, float rot)
-	{
-		this.texture = texture;
-		this.texture45 = texture45;
-		this.width = width;
-		this.height = height;
-		this.x = x;
-		this.y = y;
-		this.hp = 100;
-		this.rot = rot;
+    private Texture spriteTexture;
 
-		this.sprite = new Sprite(texture, width, height);
-		this.sprite.setPosition(x, y);
-		this.sprite.setRotation(rot);
+    public Player(Texture texture, Texture texture45, int width, int height, int x, int y, int dir)
+    {
+        this.texture = texture;
+        this.texture45 = texture45;
+        this.width = width;
+        this.height = height;
+        this.x = x;
+        this.y = y;
+        this.hp = 100;
+        this.dir = dir;
+        this.rot = 45 * dir;
 
-		this.spriteTexture = texture;
-	}
+        this.sprite = new Sprite(texture, width, height);
+        this.sprite.setPosition(x, y);
+        this.sprite.setRotation(rot);
 
-	@Override
-	public void update()
-	{
-		this.x += this.vec.x;
-		this.y += this.vec.y;
-		this.sprite.setPosition(this.x, this.y);
-		//this.sprite.setRotation(this.rot);
-		if (((int) this.rot) % 90 == 0 && this.spriteTexture != this.texture)
-		{
-			this.sprite.setTexture(this.texture);
-			this.spriteTexture = this.texture;
-			try
-			{
-				if(((int) this.rot) / 90 == 1)
-				{
-					this.sprite.setRotation(90);
-				}
-				if(((int) this.rot) / 90 == 2)
-				{
-					this.sprite.setRotation(180);
-				}
-				if(((int) this.rot) / 90 == 3)
-				{
-					this.sprite.setRotation(270);
-				}
-			}
-			catch (Exception e)
-			{
-				this.sprite.setRotation(0);
-			}
-		}
-		else if(((int) this.rot) % 45 == 0 && this.spriteTexture != this.texture45 && ((int) this.rot) != 0)
-		{
-			this.sprite.setTexture(this.texture45);
-			this.spriteTexture = this.texture45;
-			try
-			{
-				if(((int) this.rot) / 45 == 2)
-				{
-					this.sprite.setRotation(90);
-				}
-				if(((int) this.rot) / 45 == 3)
-				{
-					this.sprite.setRotation(180);
-				}
-				if(((int) this.rot) / 45 == 4)
-				{
-					this.sprite.setRotation(270);
-				}
-			}
-			catch (Exception e)
-			{
-				this.sprite.setRotation(0);
-			}
-		}
+        this.spriteTexture = texture;
+    }
 
+    @Override
+    public void update()
+    {
+        super.update();
+        if (this.dir % 2 == 0)
+        {
+            if (this.spriteTexture != this.texture)
+            {
+                this.sprite.setTexture(this.texture);
+                this.spriteTexture = this.texture;
+            }
+            if (this.dir == 0)
+            {
+                this.sprite.setRotation(0);
+            } else if (this.dir == 2)
+            {
+                this.sprite.setRotation(90);
+            } else if (this.dir == 4)
+            {
+                this.sprite.setRotation(180);
+            } else if (this.dir == 6)
+            {
+                this.sprite.setRotation(270);
+            }
+        } else
+        {
+            if (this.spriteTexture != this.texture45)
+            {
+                this.sprite.setTexture(this.texture45);
+                this.spriteTexture = this.texture45;
+            }
+            if (this.dir == 1)
+            {
+                this.sprite.setRotation(0);
+            } else if (this.dir == 3)
+            {
+                this.sprite.setRotation(90);
+            } else if (this.dir == 5)
+            {
+                this.sprite.setRotation(180);
+            } else if (this.dir == 7)
+            {
+                this.sprite.setRotation(270);
+            }
+        }
 
-
-
-		if (this.hp >= 0)
-		{
-			//this.die();
-		}
-
-		if (Gdx.input.isKeyJustPressed(Input.Keys.A))
-		{
-			this.rot += 45;
-		}
-		if (Gdx.input.isKeyJustPressed(Input.Keys.D))
-		{
-			this.rot -= 45;
-		}
-		if (this.rot >= 360)
-		{
-			this.rot -= 360;
-		}
-		if (this.rot < 0)
-		{
-			this.rot *= -1;
-		}
-		if (Gdx.input.isKeyPressed(Input.Keys.W))
-		{
-			this.tmpVec.set(10 * Gdx.graphics.getDeltaTime(), 10 * Gdx.graphics.getDeltaTime());
-			this.tmpVec.setAngle(this.rot + 90);
-			this.vec.add(this.tmpVec);
-		}
-	}
+        if (Gdx.input.isKeyJustPressed(Input.Keys.RIGHT))
+        {
+            this.dir = (dir + 7) % 8;
+        }
+        if (Gdx.input.isKeyJustPressed(Input.Keys.LEFT))
+        {
+            this.dir = (dir + 1) % 8;
+        }
+        this.rot = dir * 45;
+        if (Gdx.input.isKeyPressed(Input.Keys.UP))
+        {
+            this.acceleration.set(10 * Gdx.graphics.getDeltaTime(), 10 * Gdx.graphics.getDeltaTime());
+            this.acceleration.setAngle(this.rot);
+            this.velocity.add(this.acceleration);
+        }
+    }
 }
--- core/src/com/github/thenoofclan/orbitaltrucker/Ship.java
+++ core/src/com/github/thenoofclan/orbitaltrucker/Ship.java
@@ -8,96 +8,95 @@ import com.badlogic.gdx.math.Vector2;
 
 public class Ship
 {
-	public Texture texture;
-	public Texture texture45;
-	public Sprite sprite;
-	//private Rectangle rectangle;
-	public Sound engine;
-	public Sound die;
-	public Sound talk;
+    public Texture texture;
+    public Texture texture45;
+    public Sprite sprite;
+    // private Rectangle rectangle;
+    public Sound engine;
+    public Sound die;
+    public Sound talk;
 
-	public int width;
-	public int height;
-	public int x;
-	public int y;
-	public float rot;
-	public Vector2 vec;
-	public Vector2 tmpVec;
+    public int width;
+    public int height;
+    public int x;
+    public int y;
+    public float rot;
+    public int dir;
+    public Vector2 velocity;
+    public Vector2 acceleration;
 
-	public int hp;
+    public int hp;
 
+    public Ship(Texture texture, Texture texture45, int width, int height, int x, int y, int dir)
+    {
+        this.texture = texture;
+        this.texture45 = texture45;
+        this.width = width;
+        this.height = height;
+        this.x = x;
+        this.y = y;
+        this.hp = 100;
+        this.dir = dir;
+        this.rot = 45 * dir;
+        this.engine = null;
+        this.die = null;
+        this.talk = null;
+        this.velocity = new Vector2(0, 0);
+        this.acceleration = new Vector2(this.velocity);
 
-	public Ship(Texture texture, Texture texture45, int width, int height, int x, int y, float rot)
-	{
-		this.texture = texture;
-		this.texture45 = texture45;
-		this.width = width;
-		this.height = height;
-		this.x = x;
-		this.y = y;
-		this.hp = 100;
-		this.rot = rot;
-		this.engine = null;
-		this.die = null;
-		this.talk = null;
-		this.vec = new Vector2(0, 0);
-		this.tmpVec = new Vector2(this.vec);
+        this.sprite = new Sprite(texture, width, height);
+        this.sprite.setPosition(x, y);
+        this.sprite.setRotation(rot);
+    }
 
-		this.sprite = new Sprite(texture, width, height);
-		this.sprite.setPosition(x, y);
-		this.sprite.setRotation(rot);
-	}
+    public Ship()
+    {
+        this.texture = null;
+        this.texture45 = null;
+        this.width = 16;
+        this.height = 16;
+        this.x = 0;
+        this.y = 0;
+        this.hp = 100;
+        this.rot = 0;
+        this.engine = null;
+        this.die = null;
+        this.talk = null;
+        this.velocity = new Vector2(0, 0);
+        this.acceleration = new Vector2(this.velocity);
 
-	public Ship()
-	{
-		this.texture = null;
-		this.texture45 = null;
-		this.width = 16;
-		this.height = 16;
-		this.x = 0;
-		this.y = 0;
-		this.hp = 100;
-		this.rot = 0;
-		this.engine = null;
-		this.die = null;
-		this.talk = null;
-		this.vec = new Vector2(0, 0);
-		this.tmpVec = new Vector2(this.vec);
+        this.sprite = null;
+    }
 
-		this.sprite = null;
-	}
+    public void dispose()
+    {
+        this.texture.dispose();
+        this.engine.dispose();
+        this.die.dispose();
+        this.talk.dispose();
+    }
 
-	public void dispose()
-	{
-		this.texture.dispose();
-		this.engine.dispose();
-		this.die.dispose();
-		this.talk.dispose();
-	}
+    public void drop()
+    {
 
-	public void drop()
-	{
+    }
 
-	}
+    // public void die()
+    // {
+    // this.die.play();
+    // this.drop();
+    // this.dispose();
+    // }
 
-//	public void die()
-//	{
-//		this.die.play();
-//		this.drop();
-//		this.dispose();
-//	}
+    public void update()
+    {
+        this.x += ((int) this.velocity.x);
+        this.y += ((int) this.velocity.y);
+        this.sprite.setPosition(this.x, this.y);
 
-	public void update()
-	{
-		this.vec.setAngle(this.rot);
-		this.x += ((int) this.vec.x);
-		this.y += ((int) this.vec.y);
-		this.sprite.setPosition(this.x, this.y);
-		this.sprite.setRotation(this.rot);
-
-		if (this.hp >= 0)
-		{
-			//this.die();
-		}
-	}
+        if (this.hp >= 0)
+        {
+            // this.die();
+        }
+    }
 }
